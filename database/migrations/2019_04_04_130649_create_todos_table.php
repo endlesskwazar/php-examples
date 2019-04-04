51b329e62de7cf247a1ad0cd7c2c51d3a14fdbe5
endlesskwazar <endlesskwazar@gmail.com>
2019-04-04 16:18:41 +0300

    initial commit

---
+++ database/migrations/2019_04_04_130649_create_todos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todoes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->date('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todoes');
+    }
+}
